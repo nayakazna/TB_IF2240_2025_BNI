165fcc769cd6bad06b77d772240a5495a1fa2cf7
mineraleee <helenigratia@gmail.com>
2025-05-15 00:23:44 +0700

    feat: create initial tables

--- src/Milestone2_BNI_The Garuda Pancasila Spirit of Brr Brr Patapim.sql
+++ src/Milestone2_BNI_The Garuda Pancasila Spirit of Brr Brr Patapim.sql
@@ -1,9 +1,9 @@
 /*M!999999\- enable the sandbox mode */ 
--- MariaDB dump 10.19  Distrib 10.6.21-MariaDB, for debian-linux-gnu (x86_64)
+-- MariaDB dump 10.19-11.7.2-MariaDB, for Win64 (AMD64)
 --
--- Host: localhost    Database: IF2240_BNI
+-- Host: localhost    Database: if2240_bni
 -- ------------------------------------------------------
--- Server version	10.6.21-MariaDB-0ubuntu0.22.04.2
+-- Server version	11.7.2-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,7 +14,246 @@
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
+
+--
+-- Table structure for table `janji_temu`
+--
+
+DROP TABLE IF EXISTS `janji_temu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `janji_temu` (
+  `id_janji_temu` int(11) NOT NULL AUTO_INCREMENT,
+  `id_rumah_sakit` int(11) NOT NULL,
+  `waktu_pelaksanaan` datetime NOT NULL,
+  `alasan` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_janji_temu`,`id_rumah_sakit`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `janji_temu`
+--
+
+LOCK TABLES `janji_temu` WRITE;
+/*!40000 ALTER TABLE `janji_temu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `janji_temu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `layanan_medis`
+--
+
+DROP TABLE IF EXISTS `layanan_medis`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `layanan_medis` (
+  `id_rumah_sakit` int(11) NOT NULL,
+  `id_layanan` int(11) NOT NULL,
+  `nama_layanan` enum('vaksinasi','fisioterapi') NOT NULL,
+  PRIMARY KEY (`id_rumah_sakit`,`id_layanan`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `layanan_medis`
+--
+
+LOCK TABLES `layanan_medis` WRITE;
+/*!40000 ALTER TABLE `layanan_medis` DISABLE KEYS */;
+/*!40000 ALTER TABLE `layanan_medis` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `melakukan_janji`
+--
+
+DROP TABLE IF EXISTS `melakukan_janji`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `melakukan_janji` (
+  `email_pesan` varchar(255) NOT NULL,
+  `id_janji_temu` int(11) NOT NULL,
+  `email_tenaga_medis` varchar(255) NOT NULL,
+  `id_rumah_sakit` int(11) NOT NULL,
+  PRIMARY KEY (`email_pesan`,`id_janji_temu`,`email_tenaga_medis`,`id_rumah_sakit`),
+  KEY `id_janji_temu` (`id_janji_temu`,`id_rumah_sakit`),
+  CONSTRAINT `melakukan_janji_ibfk_1` FOREIGN KEY (`id_janji_temu`, `id_rumah_sakit`) REFERENCES `janji_temu` (`id_janji_temu`, `id_rumah_sakit`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `melakukan_janji`
+--
+
+LOCK TABLES `melakukan_janji` WRITE;
+/*!40000 ALTER TABLE `melakukan_janji` DISABLE KEYS */;
+/*!40000 ALTER TABLE `melakukan_janji` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `obat`
+--
+
+DROP TABLE IF EXISTS `obat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `obat` (
+  `id_obat` int(11) NOT NULL,
+  `label` enum('analgesik','antibiotik','obat herbal') NOT NULL,
+  PRIMARY KEY (`id_obat`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `obat`
+--
+
+LOCK TABLES `obat` WRITE;
+/*!40000 ALTER TABLE `obat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `obat` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pemesanan_layanan`
+--
+
+DROP TABLE IF EXISTS `pemesanan_layanan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pemesanan_layanan` (
+  `id_pemesanan` int(11) NOT NULL AUTO_INCREMENT,
+  `id_rumah_sakit` int(11) NOT NULL,
+  `id_layanan` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `jenis_layanan` varchar(255) NOT NULL,
+  `jadwal_pelaksanaan` date NOT NULL,
+  `status_pemesanan` enum('belum dibayar','dijadwalkan','sedang berlangsung','selesai','dibatalkan') NOT NULL,
+  `total_biaya` int(11) NOT NULL CHECK (`total_biaya` >= 0),
+  PRIMARY KEY (`id_pemesanan`,`id_rumah_sakit`,`id_layanan`,`email`),
+  KEY `id_rumah_sakit` (`id_rumah_sakit`,`id_layanan`),
+  CONSTRAINT `pemesanan_layanan_ibfk_1` FOREIGN KEY (`id_rumah_sakit`, `id_layanan`) REFERENCES `layanan_medis` (`id_rumah_sakit`, `id_layanan`),
+  CONSTRAINT `pemesanan_layanan_ibfk_2` FOREIGN KEY (`id_rumah_sakit`, `id_layanan`) REFERENCES `layanan_medis` (`id_rumah_sakit`, `id_layanan`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pemesanan_layanan`
+--
+
+LOCK TABLES `pemesanan_layanan` WRITE;
+/*!40000 ALTER TABLE `pemesanan_layanan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pemesanan_layanan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pemesanan_obat`
+--
+
+DROP TABLE IF EXISTS `pemesanan_obat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pemesanan_obat` (
+  `email` varchar(255) NOT NULL,
+  `waktu_pemesanan` datetime NOT NULL,
+  `alamat_pengiriman` varchar(255) NOT NULL,
+  `status_pemesanan` enum('belum dibayar','dijadwalkan','sedang berlangsung','selesai','dibatalkan') NOT NULL,
+  `total_biaya` int(11) NOT NULL CHECK (`total_biaya` >= 0),
+  PRIMARY KEY (`email`,`waktu_pemesanan`,`alamat_pengiriman`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pemesanan_obat`
+--
+
+LOCK TABLES `pemesanan_obat` WRITE;
+/*!40000 ALTER TABLE `pemesanan_obat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pemesanan_obat` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pemesanan_obat_obat`
+--
+
+DROP TABLE IF EXISTS `pemesanan_obat_obat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pemesanan_obat_obat` (
+  `email` varchar(255) NOT NULL,
+  `waktu_pemesanan` datetime NOT NULL,
+  `alamat_pengiriman` varchar(255) NOT NULL,
+  `id_obat` int(11) NOT NULL,
+  PRIMARY KEY (`email`,`waktu_pemesanan`,`alamat_pengiriman`,`id_obat`),
+  KEY `id_obat` (`id_obat`),
+  CONSTRAINT `pemesanan_obat_obat_ibfk_1` FOREIGN KEY (`email`, `waktu_pemesanan`, `alamat_pengiriman`) REFERENCES `pemesanan_obat` (`email`, `waktu_pemesanan`, `alamat_pengiriman`),
+  CONSTRAINT `pemesanan_obat_obat_ibfk_2` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id_obat`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pemesanan_obat_obat`
+--
+
+LOCK TABLES `pemesanan_obat_obat` WRITE;
+/*!40000 ALTER TABLE `pemesanan_obat_obat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pemesanan_obat_obat` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pengeluaran_resep`
+--
+
+DROP TABLE IF EXISTS `pengeluaran_resep`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pengeluaran_resep` (
+  `id_janji_temu` int(11) NOT NULL,
+  `id_rumah_sakit` int(11) NOT NULL,
+  `id_resep` int(11) NOT NULL,
+  PRIMARY KEY (`id_janji_temu`,`id_rumah_sakit`,`id_resep`),
+  UNIQUE KEY `id_resep` (`id_resep`),
+  CONSTRAINT `pengeluaran_resep_ibfk_1` FOREIGN KEY (`id_janji_temu`, `id_rumah_sakit`) REFERENCES `janji_temu` (`id_janji_temu`, `id_rumah_sakit`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pengeluaran_resep`
+--
+
+LOCK TABLES `pengeluaran_resep` WRITE;
+/*!40000 ALTER TABLE `pengeluaran_resep` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pengeluaran_resep` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `resep_obat`
+--
+
+DROP TABLE IF EXISTS `resep_obat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `resep_obat` (
+  `id_obat` int(11) NOT NULL,
+  `id_resep` int(11) NOT NULL,
+  `dosis` varchar(255) DEFAULT NULL,
+  `cara_pakai` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_obat`,`id_resep`),
+  KEY `id_resep` (`id_resep`),
+  CONSTRAINT `resep_obat_ibfk_1` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id_obat`),
+  CONSTRAINT `resep_obat_ibfk_2` FOREIGN KEY (`id_resep`) REFERENCES `pengeluaran_resep` (`id_resep`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `resep_obat`
+--
+
+LOCK TABLES `resep_obat` WRITE;
+/*!40000 ALTER TABLE `resep_obat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `resep_obat` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -50,6 +289,6 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
 
--- Dump completed on 2025-05-14 17:50:30
+-- Dump completed on 2025-05-15  0:10:35
